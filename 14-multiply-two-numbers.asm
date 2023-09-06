fb6b6563a742195d0483ac7a08576b0e9b97eb5f
MdTehseenKhan <tehseenk435@gmail.com>
2023-09-06 22:06:41 +0500

    Lab 07

---
+++ 14-multiply-two-numbers.asm
@@ -0,0 +1,37 @@
+; Assembly Code to Multiply two numbers
+; B21110106058 - Muhammad Tehseen Khan
+
+.model small
+.stack 100h
+.data
+.code
+main proc
+
+; Numbers to Multiply
+mov al, 3
+mov bl, 2
+
+; Multiply Operation
+mul bl
+aam   ; ASCII Adjust after multiplication
+
+mov ch, ah
+mov cl, al
+
+; Print Result
+mov dl, ch
+add dl, 48
+mov ah, 2
+int 21h
+
+mov dl, cl
+add dl, 48
+mov ah, 2
+int 21h
+
+
+mov ah, 4ch
+int 21h
+
+main endp
+end main
